b84c1636e948e7fb3f1f20ba634393bb5d89405f
user <user@LAPTOP-OGP1RD6T>
2024-09-19 18:40:33 +0700

    pekan1

---
+++ laundryapps/src/ui/LoginFrame.java
@@ -0,0 +1,97 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.user;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class LoginFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtUsername;
+	private JTextField txtPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginFrame frame = new LoginFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 467);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Laundry Apps");
+		lblNewLabel.setFont(new Font("Wide Latin", Font.ITALIC, 14));
+		lblNewLabel.setBounds(53, 36, 196, 33);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("wahai pemalas,sini kami cuciin");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(52, 79, 257, 23);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("USERNAME");
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(49, 112, 84, 23);
+		contentPane.add(lblNewLabel_2);
+		
+		txtUsername = new JTextField();
+		txtUsername.setBounds(53, 145, 277, 44);
+		contentPane.add(txtUsername);
+		txtUsername.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("PASSWORD");
+		lblNewLabel_3.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblNewLabel_3.setBounds(53, 199, 84, 23);
+		contentPane.add(lblNewLabel_3);
+		
+		txtPassword = new JTextField();
+		txtPassword.setBounds(54, 244, 276, 61);
+		contentPane.add(txtPassword);
+		txtPassword.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(user.login(txtUsername.getText(), txtPassword.getText())) {
+					new MainFrame().setVisible(true);
+					dispose();
+				}else {
+				}
+			}
+		});
+		btnLogin.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnLogin.setBounds(53, 339, 277, 44);
+		contentPane.add(btnLogin);
+	}
+}
---
+++ laundryapps/src/ui/MainFrame.java
@@ -0,0 +1,84 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 511, 402);
+		getContentPane().setLayout(null);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Laundry Apps");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 28));
+		lblNewLabel.setForeground(new Color(255, 0, 255));
+		lblNewLabel.setBounds(21, 26, 237, 63);
+		getContentPane().add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("PESANAN");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton.setBounds(31, 99, 134, 56);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("LAYANAN");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_1.setBounds(175, 99, 125, 56);
+		getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("PELANGGAN");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_2.setBounds(323, 99, 118, 56);
+		getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("PENGGUNA");
+		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_3.setBounds(32, 181, 133, 56);
+		getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("LAPORAN");
+		btnNewButton_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_4.setBounds(175, 181, 125, 56);
+		getContentPane().add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("PROFILE");
+		btnNewButton_5.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_5.setBounds(324, 181, 117, 56);
+		getContentPane().add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("KELUAR");
+		btnNewButton_6.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_6.setBounds(31, 285, 411, 47);
+		getContentPane().add(btnNewButton_6);
+	}
+
+}
