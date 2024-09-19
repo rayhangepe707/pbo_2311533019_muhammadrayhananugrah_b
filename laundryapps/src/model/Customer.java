2317e3dec0cb233f0f054d791e6473640711b3ef
user <user@LAPTOP-OGP1RD6T>
2024-09-19 19:08:54 +0700

    pekan2

---
+++ laundryapps/src/model/Customer.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Customer {
+	String id, nama, alamat, nomorhp;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getAlamat() {
+		return alamat;
+	}
+
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+
+	public String getNomorhp() {
+		return nomorhp;
+	}
+
+	public void setNomorhp(String nomorhp) {
+		this.nomorhp = nomorhp;
+	}
+
+}
